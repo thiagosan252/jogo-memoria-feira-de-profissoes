a3e809ae652f78ca032bc9267845589afb6f71b1
Thiago Santana <thiagosan252@gmail.com>
2020-12-27 17:34:42 -0300

    add routes in index.js e apply css in index.html

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Switch, MemoryRouter } from "react-router-dom";
+
 import './index.css';
-import App from './App';
+import Home from './pages/home/Home';
+import Game from './pages/game/Game';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MemoryRouter>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/jogar' component={Game} />
+        
+      </Switch>
+    </MemoryRouter>,
   </React.StrictMode>,
   document.getElementById('root')
 );
