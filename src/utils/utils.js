ad3891d57b8bac5775b602daa98fe96e6c16b991
Thiago Santana <thiagosan252@gmail.com>
2020-12-29 17:43:53 -0300

    added function to transform seconds in hh:mm:ss

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -19,3 +19,15 @@ export const shuffleArray = (array) => {
 
     return newArray;
 }
+
+export const secondsTohhmmss = (totalSeconds) => {
+    var hours = Math.floor(totalSeconds / 3600);
+    var minutes = Math.floor((totalSeconds - hours * 3600) / 60);
+    var seconds = totalSeconds - hours * 3600 - minutes * 60; // round seconds
+
+    seconds = Math.round(seconds * 100) / 100;
+    var result = hours < 10 ? "0" + hours : hours;
+    result += ":" + (minutes < 10 ? "0" + minutes : minutes);
+    result += ":" + (seconds < 10 ? "0" + Math.trunc(seconds) : Math.trunc(seconds));
+    return result;
+}
