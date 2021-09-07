0bcc93f6ccc8af4629475bf51ac420ac7f59e6d8
Thiago Santana <thiagosan252@gmail.com>
2021-09-07 20:45:55 -0300

    fix: responsividade da pagina home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -13,18 +13,16 @@ function Home({ history }) {
 
 
     return (
-        <Container fluid className="h-100 scrollbar-auto">
+        <Container fluid className="h-100">
             <Row className="d-flex justify-content-center align-itens-center h-100">
-                <Col className="align-self-center text-center">
-                    <div className="my-3">
-                        <img src={process.env.PUBLIC_URL + '/images/home/logo.jpeg'} alt="Logo do curso de Nutrição" height="300" width="300" />
-                        <div className="py-4">
-                            <p className="h1 text-dark">Feira de Profissões UEL - 2021</p>
-                            <p className="h5 text-muted">Jogo da Memória</p>
-                        </div>
+                <div className="col align-self-center text-center">
+                    <img className="img-fluid" src={process.env.PUBLIC_URL + '/images/home/logo.jpeg'} alt="Logo do curso de Nutrição" height="300" width="300" />
+                    <div className="py-4">
+                        <p className="h1 text-dark">Feira de Profissões UEL - 2021</p>
+                        <p className="h5 text-muted">Jogo da Memória</p>
                     </div>
                     <Button className="btn-shadow" color="secondary" size="lg" onClick={() => history.push('/jogo')}>Iniciar o Jogo</Button>
-                </Col>
+                </div>
             </Row>
         </Container >
 
